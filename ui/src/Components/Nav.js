076c922892185cf15858fab99025196af15d44b4
CurlyRishabh <rissag940@gmail.com>
2023-11-05 03:11:15 +0530

    nav mobile and desktop complete

--- ui/src/Components/Nav.js
+++ ui/src/Components/Nav.js
@@ -1,4 +1,69 @@
 import React, {useState} from 'react'
+import menu from "../Media/nav.png"
+
+function Mobile(props){
+	return (
+		<div className={`md:hidden  flex flex-col  transition-all ease-out duration-1000 ${props.show ? "w-28" : "w-20"}`}>
+			<div className=" text-white items-center flex font-mono font-bold justify-between  text-3xl gap-2  ">
+				<img className="h-8" src={menu} alt="///" onClick={props.handleClick} />
+				<p>
+					R<span className="font-thin text-white text-2xl pr-1">SB</span>
+				</p>
+			</div>
+			<div className={" pl-2 text-xl  bg-violet-500  " + (props.show ? "flex flex-col h-36 border-t-2 border-violet-600" : "hidden")}>
+				<a
+					onClick={props.handleClick}
+					href="#about"
+					className=" text-white hover:text-gray-400  border-b pr-2 mr-4 pb-2 "
+				>
+					About
+				</a>
+				<a
+					onClick={props.handleClick}
+					href="#projects"
+					className=" text-white hover:text-gray-400  border-b pr-2 mr-4 pb-2"
+				>
+					Projects
+				</a>
+				<a
+					onClick={props.handleClick}
+					href="#contact"
+					className=" text-white hover:text-gray-400 border-b pr-2 mr-4 pb-2 "
+				>
+					Contact
+				</a>
+				<a onClick={props.handleClick} href="#resume" className=" text-white hover:text-gray-400">
+					Resume
+				</a>
+			</div>
+		</div>
+	);
+}
+
+function Desktop(){
+	return (
+		<div className="hidden md:flex px-3 items-center justify-between">
+			<div className="text-white font-mono font-bold  text-3xl">
+				R<span className="font-thin text-white text-2xl">SB</span>
+			</div>
+			<div className="space-x-4 ">
+				<a href="#about" className="text-white hover:text-gray-400 ">
+					About
+				</a>
+				<a href="#projects" className="text-white hover:text-gray-400">
+					Projects
+				</a>
+				<a href="#contact" className="text-white hover:text-gray-400 ">
+					Contact
+				</a>
+				<a href="#resume" className="text-white hover:text-gray-400">
+					Resume
+				</a>
+			</div>
+		</div>
+	);
+}
+ 
 
 export default function Nav() {
 	const [showMenu, setShowMenu] = useState(false);
@@ -8,47 +73,9 @@ export default function Nav() {
 	};
 
   return (
-		// <nav className="flex px-2 justify-between h-14 items-center fixed top-0 left-0 w-full bg-blue-600">
-		// 	<p>RSB</p>
-		// 	<div className="flex">
-		// 		<div className=" flex flex-col relative right-0 w-10 h-10 ">
-		// 			<div>++</div>
-		// 			<a href="#about">About</a>
-		// 			<a href="#projects">Project</a>
-		// 			<a href="#contact">Contact</a>
-		// 			<a href="#contact">Resume</a>
-		// 		</div>
-		// 	</div>
-		// </nav>
-
-		<nav className="bg-gray-800 p-4">
-			<div className="flex items-center justify-between">
-				<div className="flex items-center flex-shrink-0 text-white mr-6" onClick={toggleMenu}>
-					<span className="font-semibold text-xl tracking-tight cursor-pointer">Logo</span>
-				</div>
-				<div className={`w-full block lg:flex lg:items-center lg:w-auto ${showMenu ? "block" : "hidden"}`}>
-					<div className="text-sm lg:flex-grow">
-						<a href="#about" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4">
-							About
-						</a>
-						<a
-							href="#projects"
-							className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4"
-						>
-							Projects
-						</a>
-						<a
-							href="#contact"
-							className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4"
-						>
-							Contact
-						</a>
-						<a href="#resume" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400">
-							Resume
-						</a>
-					</div>
-				</div>
-			</div>
+		<nav className="fixed h-10 w-full bg-violet-500 ">
+			<Mobile handleClick={toggleMenu} show={showMenu}/>
+			<Desktop />
 		</nav>
   );
 }
